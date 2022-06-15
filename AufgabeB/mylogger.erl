6c9b3ac45e10bf313f815aa3e53f5b869faac021
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-06-15 17:39:37 +0200

    Create mylogger.erl

---
+++ AufgabeB/mylogger.erl
@@ -0,0 +1,23 @@
+-module(mylogger).
+-export([start/1, stop/1]).
+
+start(Nodes) ->
+  spawn_link(fun() -> init(Nodes) end).
+
+stop(Logger) ->
+  Logger ! stop.
+
+init(_) ->
+  loop().
+
+loop() ->
+  receive {log, From, Time, Msg} ->
+      log(From, Time, Msg),
+      loop();
+    stop ->
+      ok
+end.
+
+log(From, Time, Msg) ->
+  io:format("log:", {Time, From, Msg}). 
+%TO DO Zeile 22
