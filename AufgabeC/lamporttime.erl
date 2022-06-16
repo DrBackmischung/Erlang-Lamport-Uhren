52604d7b8b46deba3392546d3c6cc18d8e9b9411
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-06-16 14:06:31 +0200

    Update lamporttime.erl

--- AufgabeC/lamporttime.erl
+++ AufgabeC/lamporttime.erl
@@ -1,10 +1,7 @@
 -modul(lamporttime).
 -export([zero/0, inc/1, merge/2, leq/2]).
 
-zero() -> 0.
-
-inc(T) -> T + 1.
-
-merge(Ti, Tj) -> erlang:max(Ti, Tj).
-
-leq(Ti, Tj) -> Ti =< Tj.
+zero() -> ok.
+inc(T) -> ok.
+merge(Ti, Tj) -> ok.
+leq(Ti, Tj) -> ok.
