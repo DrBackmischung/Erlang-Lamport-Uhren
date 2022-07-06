06c391fd149790e6d023525bd9ed8ade3bf5b59d
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-07-07 00:25:48 +0200

    :)))

--- AufgabeD/test.erl
+++ AufgabeD/test.erl
@@ -11,7 +11,7 @@ run(Sleep, Jitter) ->
   worker:peers(B, [A, C, D]),
   worker:peers(C, [A, B, D]),
   worker:peers(D, [A, B, C]),
-  timer:sleep(10000),
+  timer:sleep(5000),
   mylogger:stop(Log),
   worker:stop(A),
   worker:stop(B),
