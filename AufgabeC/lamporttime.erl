a95efc6d93b9b08ab1602fd28726be1b2b3841a6
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-07-10 17:50:43 +0200

    %

--- AufgabeC/lamporttime.erl
+++ AufgabeC/lamporttime.erl
@@ -1,10 +1,17 @@
+% Moduldefinition
 -module(lamporttime).
+
+% Exportierte Methoden
 -export([zero/0, inc/1, merge/2, leq/2]).
 
+% Gibt den initialen Zeitstempel zurück: 0
 zero() -> 0.
 
+% Zählt einen Zeitstempel um 1 hoch
 inc(T) -> T + 1.
 
+% Gibt den größeren von zwei Zeitstempeln zurück
 merge(Ti, Tj) -> erlang:max(Ti, Tj).
 
+% Gibt zurück, ob ein Zeitstempel kleiner gleich einem zweiten Zeitstempel ist
 leq(Ti, Tj) -> Ti =< Tj.
