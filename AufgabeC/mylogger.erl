bc7218bef2669958c27f18e4017f0355da8f4c13
Mathis Neunzig <mathis.neunzig@gmail.com>
2022-07-10 20:32:43 +0200

    log my logger aufgabe c

--- AufgabeC/mylogger.erl
+++ AufgabeC/mylogger.erl
@@ -1,22 +1,33 @@
+% Moduldefinition
 -module(mylogger).
+
+% Exportierte Methoden
 -export([start/1, stop/1]).
 
+% Startet den Logger
 start(Nodes) ->
   spawn_link(fun() -> init(Nodes) end).
 
+% Stoppt den Logger
 stop(Logger) ->
   Logger ! stop.
 
+% Initialisiert den Logger
 init(_) ->
   loop().
 
+% Gibt durchgehend ankommende Log-Nachrichten aus
 loop() ->
-  receive {log, From, Time, Msg} ->
+  receive
+    % Beim Empfangen eines Logs wird dieser ausgegeben und erneut mit loop auf neue Logs gewartet
+    {log, From, Time, Msg} ->
       log(From, Time, Msg),
       loop();
+    % Wenn "stop" empfangen wird, wird "ok" ausgegeben und der loop läuft nicht erneut durch
     stop ->
       ok
 end.
 
+% Loggt die Nachricht im Standard-Output (zB Terminal)
 log(From, Time, Msg) ->
   io:format("log: ~w ~w ~p~n", [Time, From, Msg]). 
