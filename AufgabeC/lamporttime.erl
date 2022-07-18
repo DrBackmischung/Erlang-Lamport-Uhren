6f55bbf9b45b80d818d2ef35ecc9916df45e3d31
Tomke Müller <78802953+Tsawlen@users.noreply.github.com>
2022-07-18 09:33:58 +0200

    Add files via upload

--- AufgabeC/lamporttime.erl
+++ AufgabeC/lamporttime.erl
@@ -1,24 +1,24 @@
 -module(lamporttime).
 -export([zero/0, inc/1, merge/2,leq/2]).
 
-% Just a plain function returning 0
+% Eine einfache Funktion, welche nur den Wert '0' zurückgibt 
 zero() ->
     0.
 
-% Just a simple function incrementing a given number
+% Diese Funktion nimmt eine Zahl entgegen und gibt diese inkrementiert zurück
 inc(T) ->
     T + 1.
 
-% A function which compares two values and returns the greater value
+% Diese Funktion vergleicht zwei übergebende Werte und gibt den größeren zurück
 merge(Ti, Tj) ->
    if 
-        % If Ti is greater than Tj, return Ti
+        % So Ti größer als Tj ist, gebe Ti zurück.
         Ti > Tj -> Ti;
-        % If Ti is less than Tj, return Tj
+        % So Ti kleiner als Tj ist, gebe Tj zurück.
         Ti < Tj -> Tj;
-        % If the values are the same (and for all other cases), return Ti
+        % Wenn beide Werte gleich groß sind, so gebe Ti zurück.
         true -> Ti
    end.
 
-% Return true, if Ti is smaller or equal to Tj, otherwise return false
+% Diese Funktion gibt zurück, ob Ti kleiner oder gleich Tj ist.
 leq(Ti, Tj) -> (Ti =< Tj).
