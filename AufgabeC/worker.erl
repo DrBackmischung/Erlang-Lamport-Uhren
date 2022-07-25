3e190c85e1975211ea5a518b8047521841e1ab8a
Tsawlen <78802953+Tsawlen@users.noreply.github.com>
2022-07-25 18:42:00 +0200

    Kommentiere Worker von C

--- AufgabeC/worker.erl
+++ AufgabeC/worker.erl
@@ -1,6 +1,8 @@
 -module(worker).
 -export([start/4, stop/1, peers/2]).
 
+% Der Code ist bereits ausführlich in der Teilaufgabe B) kommentiert. Hier werden nur die neuen Teile kommentiert.
+
 start(Name, Logger, Sleep, Jitter) ->
   spawn_link(fun() -> init(Name, Logger, Sleep, Jitter) end).
 
@@ -18,12 +20,17 @@ end.
 peers(Wrk, Peers) ->
   Wrk ! {peers, Peers}.
 
+% Im Unterschied zur B wird hier der Funktion die aktuelle Systemzeit (im Lamportzeit-Format) übergeben.
 loop(Name, Log, Peers, Sleep, Jitter, LocalTime) ->
   Wait = rand:uniform(Sleep),
   receive
     {msg, Time, Msg} ->
+      % Wenn eine Nachricht empfangen wird, vergleiche die mitgesendete Lamportzeit mit der lokalen, wähle die höhere und inkrementiere diese 
+      % und setzte diese als die neue lokale Zeit.
       NewLocalTime = lamporttime:inc(lamporttime:merge(Time, LocalTime)),
+      % Übergebe die Nachricht an den Logger inklusive der neuen System-Lamportzeit
       Log ! {log, Name, NewLocalTime, {received, Msg}},
+      % Starte die Loop erneut, mit der neuen Lamportzeit
       loop(Name, Log, Peers, Sleep, Jitter, NewLocalTime);
     stop ->
           ok;
@@ -31,10 +38,14 @@ loop(Name, Log, Peers, Sleep, Jitter, LocalTime) ->
       Log ! {log, Name, time, {error, Error}}
     after Wait ->
       Selected = select(Peers),
+      % Bevor die Nachricht geschickt wird, setze den Zeitstempel aus die aktuelle Lamportzeit und inkrementiere diese.
       NewLocalTime = lamporttime:inc(LocalTime),
       Message = {hello, rand:uniform(100)},
+      % Sende die Nachricht an den ausgewählten Peer, inklusive der neuen Zeit.
       Selected ! {msg, NewLocalTime, Message},
       jitter(Jitter),
+        % Sende eine Information darüber, dass eine Nachricht gesendet wurde an den Logger, inklusive der Zeit im Feld NewLocalTime, welche 
+        % der Zeitstempel der gesendeten Nachricht ist.
         Log ! {log, Name, NewLocalTime, {sending, Message}},
         loop(Name, Log, Peers, Sleep, Jitter, NewLocalTime)
 end.
